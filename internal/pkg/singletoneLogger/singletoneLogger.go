216bbf5b879d2cbcf473be9c9b7570ccf185df94
Sinimawath <sinimawath@gmail.com>
2018-09-16 20:15:55 +0300

    Divide into 2 files

--- internal/pkg/singletoneLogger/singletoneLogger.go
+++ internal/pkg/singletoneLogger/singletoneLogger.go
@@ -4,12 +4,7 @@
 package singletoneLogger
 
 import (
-	"bufio"
-	"errors"
-	"io"
 	"log"
-	"os"
-	"strconv"
 	"sync"
 
 	"github.com/fatih/color"
@@ -21,89 +16,10 @@ func init() {
 		"red":   color.New(color.FgRed).SprintfFunc(),
 		"green": color.New(color.FgGreen).SprintfFunc(),
 	}
+
 	once.Do(func() {
 		instance = initLogger(configInstance)
 	})
-}
-
-const configFilename = "logger.json"
-
-// для чтения из конфига. Имя переменной совпадает с именем в конфиге
-type loggerConfigStruct struct {
-	OutDist    string // возможные варианты: file, stdout
-	Filename   string // название файла, если указан file
-	ColorError string // цвет ошибки
-	ColorMsg   string // цвет сообщения
-	BuffSize   string // максимальный размер каналов
-
-}
-
-type loggerInfo struct {
-	out      io.Writer                               // writer для логов
-	buffSize int                                     // максимальный размер каналов
-	errColor func(s string, a ...interface{}) string // функция окрашивающая цвет для ошибок
-	msgColor func(s string, a ...interface{}) string // функция окрашивающая цвет для сообщений
-}
-
-var (
-	errIncorrectValue = errors.New("Incorrect value")
-)
-
-func (l *loggerInfo) readFromConfig(filename string, conf config.Config) error {
-	var dst loggerConfigStruct
-	err := conf.Read(filename, &dst)
-	if err != nil {
-		return err
-	}
-
-	switch dst.OutDist {
-	case "file":
-		// TODO: Где-то закрывать файл
-		f, err := os.Create(dst.Filename)
-		if err != nil {
-			return err
-		}
-		l.out = bufio.NewWriter(f)
-
-	case "stdout":
-		l.out = os.Stdout
-	default:
-		// логируем в stdout, если логгер не создан еще
-		log.Println(errIncorrectValue.Error(), "outDst:", dst.OutDist)
-	}
-
-	fun, ok := colorFunc[dst.ColorError]
-	if !ok {
-		log.Println(errIncorrectValue.Error(), "colorError:", dst.ColorError)
-	} else {
-		l.errColor = fun
-	}
-
-	fun, ok = colorFunc[dst.ColorMsg]
-	if !ok {
-		log.Println(errIncorrectValue.Error(), "colorError:", dst.ColorError)
-	} else {
-		l.msgColor = fun
-	}
-
-	val, err := strconv.Atoi(dst.BuffSize)
-	if err != nil {
-		log.Println(errIncorrectValue.Error(), "bufSize:", dst.BuffSize)
-	} else {
-		l.buffSize = val
-	}
-
-	return nil
-}
-
-// создает loggerInfo с значениями по умолчанию
-func newLoggerInfo() *loggerInfo {
-	return &loggerInfo{
-		out:      os.Stdout,
-		buffSize: 100,
-		errColor: color.New(color.FgRed).SprintfFunc(),
-		msgColor: color.New(color.FgGreen).SprintfFunc(),
-	}
 }
 
 var (
