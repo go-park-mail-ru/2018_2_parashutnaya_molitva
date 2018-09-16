5723a46c50ef9a349c7d3ffa3475203a17a5ea89
Sinimawath <sinimawath@gmail.com>
2018-09-16 18:18:05 +0300

    Config JsonConfig

---
+++ internal/pkg/config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type Config interface {
+	Read(filename string, structure interface{}) error
+}
+
+const configsDir = "configs"
+
+// формирует полный путь до конфига
+func normalizeFilepath(filename string) (string, error) {
+	binPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return "", err
+	}
+
+	arrPath := strings.Split(binPath, string(filepath.Separator))
+	arrPath = append([]string{"/"}, arrPath...)
+	arrPath = append(arrPath[:len(arrPath)-2], configsDir)
+	arrPath = append(arrPath, filename)
+	fullPath := filepath.Join(arrPath...)
+	return fullPath, nil
+}
+
+type JsonConfig struct{}
+
+func (JsonConfig) Read(filename string, structure interface{}) error {
+	fullFileName, err := normalizeFilepath(filename)
+	if err != nil {
+		return err
+	}
+	f, err := os.Open(fullFileName)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	body, err := ioutil.ReadAll(f)
+	if err != nil {
+		return err
+	}
+
+	log.Println(string(body))
+
+	err = json.Unmarshal(body, structure)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- internal/pkg/singletoneLogger/singletoneLogger.go
+++ internal/pkg/singletoneLogger/singletoneLogger.go
@@ -4,19 +4,104 @@
 package singletoneLogger
 
 import (
-	"github.com/fatih/color"
+	"bufio"
+	"errors"
+	"io"
 	"log"
 	"os"
+	"strconv"
 	"sync"
+
+	"github.com/fatih/color"
+	"github.com/go-park-mail-ru/2018_2_parashutnaya_molitva/internal/pkg/config"
 )
 
+const configFilename = "logger.json"
+
+// для чтения из конфига. Имя переменной совпадает с именем в конфиге
+type loggerConfigStruct struct {
+	OutDist    string // возможные варианты: file, stdout
+	Filename   string // название файла, если указан file
+	ColorError string // цвет ошибки
+	ColorMsg   string // цвет сообщения
+	BuffSize   string // максимальный размер каналов
+
+}
+
+type loggerInfo struct {
+	out      io.Writer                               // writer для логов
+	buffSize int                                     // максимальный размер каналов
+	errColor func(s string, a ...interface{}) string // функция окрашивающая цвет для ошибок
+	msgColor func(s string, a ...interface{}) string // функция окрашивающая цвет для сообщений
+}
+
 var (
-	out      = os.Stdout // writer для логов
-	buffSize = 100 // максимальный размер каналов
-	red      = color.New(color.FgRed).SprintfFunc() // функция окрашивающая цвет для текста в консоли красным
-	green    = color.New(color.FgGreen).SprintFunc() // функция окрашивающая цвет для текста в консоли зеленым
-	instance *singletonLogger // инстанс синглтона
-	once     sync.Once // Магия для реализации singleton
+	errIncorrectValue = errors.New("Incorrect value")
+)
+
+func (l *loggerInfo) readFromConfig(filename string, conf config.Config) error {
+	var dst loggerConfigStruct
+	err := conf.Read(filename, &dst)
+	if err != nil {
+		return err
+	}
+
+	switch dst.OutDist {
+	case "file":
+		// TODO: Где-то закрывать файл
+		f, err := os.Create(dst.Filename)
+		if err != nil {
+			return err
+		}
+		l.out = bufio.NewWriter(f)
+
+	case "stdout":
+		l.out = os.Stdout
+	default:
+		// логируем в stdout, если логгер не создан еще
+		log.Println(errIncorrectValue.Error(), "outDst:", dst.OutDist)
+	}
+
+	fun, ok := colorFunc[dst.ColorError]
+	if !ok {
+		log.Println(errIncorrectValue.Error(), "colorError:", dst.ColorError)
+	} else {
+		l.errColor = fun
+	}
+
+	fun, ok = colorFunc[dst.ColorMsg]
+	if !ok {
+		log.Println(errIncorrectValue.Error(), "colorError:", dst.ColorError)
+	} else {
+		l.msgColor = fun
+	}
+
+	val, err := strconv.Atoi(dst.BuffSize)
+	if err != nil {
+		log.Println(errIncorrectValue.Error(), "bufSize:", dst.BuffSize)
+	} else {
+		l.buffSize = val
+	}
+
+	return nil
+}
+
+// создает loggerInfo с значениями по умолчанию
+func newLoggerInfo() *loggerInfo {
+	return &loggerInfo{
+		out:      os.Stdout,
+		buffSize: 100,
+		errColor: color.New(color.FgRed).SprintfFunc(),
+		msgColor: color.New(color.FgGreen).SprintfFunc(),
+	}
+}
+
+var (
+	colorFunc        map[string]func(s string, a ...interface{}) string
+	configInstance   = &config.JsonConfig{}
+	loggerInfoStruct = newLoggerInfo()
+	instance         *singletonLogger // инстанс синглтона
+	once             sync.Once        // Магия для реализации singleton
 )
 
 type singletonLogger struct {
@@ -30,35 +115,50 @@ func (singletonLogger *singletonLogger) startLogging() {
 	var message string
 	for {
 		select {
-		case err = <- singletonLogger.errorChan:
-			singletonLogger.logger.Println(red("%+v\n", err))
-		case message = <- singletonLogger.messageChan:
-			singletonLogger.logger.Println(green(message))
+		case err = <-singletonLogger.errorChan:
+			singletonLogger.logger.Println(loggerInfoStruct.errColor("%+v\n", err))
+		case message = <-singletonLogger.messageChan:
+			singletonLogger.logger.Println(loggerInfoStruct.msgColor(message))
 		}
 	}
 }
 
-func initLogger() *singletonLogger {
-	errorChan := make(chan error, buffSize)
-	messageChan := make(chan string, buffSize)
+func initLogger(cfg config.Config) *singletonLogger {
+	err := loggerInfoStruct.readFromConfig(configFilename, cfg)
+
+	if err != nil {
+		log.Println("While reading config", err.Error()) // если не удалось, то остаются значения по умолчанию
+	}
+
+	errorChan := make(chan error, loggerInfoStruct.buffSize)
+	messageChan := make(chan string, loggerInfoStruct.buffSize)
 	errorLogger := singletonLogger{
-		log.New(out, "", log.LstdFlags),
+		log.New(loggerInfoStruct.out, "", log.LstdFlags),
 		errorChan,
 		messageChan}
 	go errorLogger.startLogging()
 	return &errorLogger
 }
 
+// LogError - пишет в writer отдельным цветом
 func LogError(err error) {
 	once.Do(func() {
-		instance = initLogger()
+		instance = initLogger(configInstance)
 	})
 	instance.errorChan <- err
 }
 
+// LogMessage - пишет в writer отдельным цветом
 func LogMessage(message string) {
 	once.Do(func() {
-		instance = initLogger()
+		instance = initLogger(configInstance)
 	})
 	instance.messageChan <- message
+}
+
+func init() {
+	colorFunc = map[string]func(s string, a ...interface{}) string{
+		"red":   color.New(color.FgRed).SprintfFunc(),
+		"green": color.New(color.FgGreen).SprintfFunc(),
+	}
 }
