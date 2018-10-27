fa39d954a5a6eccbeb21d73d2d868ef437a8c672
Chubasik <tsygankov.maxim@gmail.com>
2018-10-27 16:22:44 +0300

    Add board

--- board.go
+++ board.go
@@ -8,12 +8,28 @@ type Board struct {
 
 func NewBoard() Board {
 	field := make([][]PieceInterface, 8)
-	for i := 0; i < 8; i++ {
+
+	field[0] = []PieceInterface{
+		NewRook(WHITE), NewKnight(WHITE), NewBishop(WHITE), NewQueen(WHITE),
+		NewKing(WHITE), NewBishop(WHITE), NewKnight(WHITE), NewRook(WHITE)}
+	for i := 1; i < 7; i++ {
 		field[i] = make([]PieceInterface, 8)
+	}
+	for i := 0; i < 8; i++ {
+		field[1][i] = NewPawn(WHITE)
+	}
+	for i := 2; i < 6; i++ {
 		for j := 0; j < 8; j++ {
-			field[i][j] = NewKing(WHITE)
+			field[i][j] = NewEmpty()
 		}
 	}
+	for i := 0; i < 8; i++ {
+		field[6][i] = NewPawn(BLACK)
+	}
+	field[7] = []PieceInterface{
+		NewRook(BLACK), NewKnight(BLACK), NewBishop(BLACK), NewQueen(BLACK),
+		NewKing(BLACK), NewBishop(BLACK), NewKnight(BLACK), NewRook(BLACK)}
+
 	return Board{field: field}
 }
 
@@ -21,14 +37,14 @@ func (b *Board) MoveUCI(uci string) {
 	b.Move(UciToCoords(uci))
 }
 
-func (b *Board) Move (rFrom, cFrom, rTo, cTo int) {
+func (b *Board) Move(rFrom, cFrom, rTo, cTo int) {
 	b.field[rTo][cTo] = b.field[rFrom][cFrom]
 	b.field[rTo][cTo].SetMoved(true)
 	b.field[rFrom][cFrom] = NewEmpty()
 }
 
 func (b *Board) PieceAt(r, c int) PieceInterface {
-	if r < 0 || r >=8 || c < 0 || c >= 8 {
+	if r < 0 || r >= 8 || c < 0 || c >= 8 {
 		return NewNone()
 	}
 	return b.field[r][c]
@@ -36,7 +52,7 @@ func (b *Board) PieceAt(r, c int) PieceInterface {
 
 func (b *Board) PrintBoard() {
 	fmt.Println(len(b.field))
-	for i := 7; i >= 0; i -- {
+	for i := 7; i >= 0; i-- {
 		for j := 0; j < 8; j++ {
 			fmt.Printf("%c", b.PieceAt(i, j).ShortName())
 		}
--- rook.go
+++ rook.go
@@ -1,1 +1,23 @@
 package chess
+
+type Rook struct {
+	Piece
+}
+
+func (p *Rook) ShortName() rune {
+	if p.color == WHITE {
+		return 'R'
+	} else {
+		return 'r'
+	}
+}
+
+func NewRook(color PieceColor) *Rook {
+	return &Rook{
+		Piece{
+			pieceType: RookType,
+			color:     color,
+			isMoved:   false,
+		},
+	}
+}
