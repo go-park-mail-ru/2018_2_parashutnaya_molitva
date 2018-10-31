7074f8dd2f87b5b0585f70e3c5b8bb34f308cdfb
Chubasik <tsygankov.maxim@gmail.com>
2018-10-31 22:38:49 +0300

    Add subtract() Add multiply()

--- structs.go
+++ structs.go
@@ -8,3 +8,11 @@ type Coord struct {
 func (c *Coord) add(o *Coord) Coord {
 	return Coord{c.r + o.r, c.c + o.c}
 }
+
+func (c *Coord) subtract(o *Coord) Coord {
+	return Coord{c.r - o.r, c.c - o.c}
+}
+
+func (c *Coord) multiply(o *Coord) Coord {
+	return Coord{c.r * o.r, c.c * o.c}
+}
