3b28db8361a694d19a29253d6840df7ae9fa598b
Chubasik <tsygankov.maxim@gmail.com>
2018-11-03 00:49:14 +0300

    Add pawn promotion

--- moves.go
+++ moves.go
@@ -2,6 +2,16 @@ package chess
 
 import "fmt"
 
+func promotionMap() map[string]PieceType {
+	pMap := map[string]PieceType{
+		"q": QueenType,
+		"r": RookType,
+		"b": BishopType,
+		"n": KnightType,
+	}
+	return pMap
+}
+
 func PawnMoves(b *Board, pos Coord, attackOnly bool) map[string]*Board {
 	availableMoves := make(map[string]*Board)
 
@@ -10,8 +20,7 @@ func PawnMoves(b *Board, pos Coord, attackOnly bool) map[string]*Board {
 	// relative coordinates
 	forward := Coord{1, 0}
 	doubleForward := Coord{2, 0}
-	leftCapture := Coord{1, -1}
-	rightCapture := Coord{1, 1}
+	capture := Coord{1, -1}
 	enPasant := Coord{-1, 0}
 	pawnEnPassantRelative := Coord{-1, 0}
 
@@ -21,8 +30,7 @@ func PawnMoves(b *Board, pos Coord, attackOnly bool) map[string]*Board {
 
 		forward = forward.multiply(reverseFactor)
 		doubleForward = doubleForward.multiply(reverseFactor)
-		leftCapture = leftCapture.multiply(reverseFactor)
-		rightCapture = rightCapture.multiply(reverseFactor)
+		capture = capture.multiply(reverseFactor)
 		enPasant = enPasant.multiply(reverseFactor)
 		pawnEnPassantRelative = pawnEnPassantRelative.multiply(reverseFactor)
 	}
@@ -30,17 +38,29 @@ func PawnMoves(b *Board, pos Coord, attackOnly bool) map[string]*Board {
 	// field coordinates
 	forward = forward.add(&pos)
 	doubleForward = doubleForward.add(&pos)
-	leftCapture = leftCapture.add(&pos)
-	rightCapture = rightCapture.add(&pos)
 	enPasant = doubleForward.add(&enPasant)
 
 	if !attackOnly {
 		pieceAtForward := b.PieceAt(forward)
 		if pieceAtForward.Type() == EmptyType {
-			moveBoard := b.Copy()
-			moveBoard.MovePiece(pos, forward)
-			moveBoard.RemoveEnPassant()
-			availableMoves[CoordsToUcis(pos, forward)] = moveBoard
+			uci := CoordsToUcis(pos, forward)
+			// promotion
+			if pawn.Color() == WHITE && forward.r == 7 ||
+				pawn.Color() == BLACK && forward.r == 0 {
+				pMap := promotionMap()
+				for pKey, pType := range pMap {
+					moveBoard := b.Copy()
+					moveBoard.MovePiece(pos, forward)
+					moveBoard.SetPieceAt(forward, NewPiece(pType, pawn.Color()))
+					moveBoard.RemoveEnPassant()
+					availableMoves[uci+pKey] = moveBoard
+				}
+			} else {
+				moveBoard := b.Copy()
+				moveBoard.MovePiece(pos, forward)
+				moveBoard.RemoveEnPassant()
+				availableMoves[uci] = moveBoard
+			}
 		}
 
 		pieceAtDoubleForward := b.PieceAt(doubleForward)
@@ -53,28 +73,37 @@ func PawnMoves(b *Board, pos Coord, attackOnly bool) map[string]*Board {
 		}
 	}
 
-	pieceAtLeftCapture := b.PieceAt(leftCapture)
-	if pieceAtLeftCapture.Color() != pawn.Color() && pieceAtLeftCapture.Color() != NONE {
-		moveBoard := b.Copy()
-		moveBoard.MovePiece(pos, leftCapture)
-		moveBoard.RemoveEnPassant()
-		if pieceAtLeftCapture.Type() == EnPassantType {
-			pawnEnPassantAbsolute := pawnEnPassantRelative.add(&leftCapture)
-			moveBoard.SetPieceAt(pawnEnPassantAbsolute, NewPiece(EmptyType, NONE))
-		}
-		availableMoves[CoordsToUcis(pos, leftCapture)] = moveBoard
-	}
+	captureMultipliers := []Coord{{1, 1}, {1, -1}}
 
-	pieceAtRightCapture := b.PieceAt(rightCapture)
-	if pieceAtRightCapture.Color() != pawn.Color() && pieceAtRightCapture.Color() != NONE {
-		moveBoard := b.Copy()
-		moveBoard.MovePiece(pos, rightCapture)
-		moveBoard.RemoveEnPassant()
-		if pieceAtRightCapture.Type() == EnPassantType {
-			pawnEnPassantAbsolute := pawnEnPassantRelative.add(&rightCapture)
-			moveBoard.SetPieceAt(pawnEnPassantAbsolute, NewPiece(EmptyType, NONE))
+	for i := 0; i < len(captureMultipliers); i++ {
+		captureAbs := capture.multiply(&captureMultipliers[i])
+		captureAbs = captureAbs.add(&pos)
+		pieceAtCapture := b.PieceAt(captureAbs)
+
+		if pieceAtCapture.Color() != pawn.Color() && pieceAtCapture.Color() != NONE {
+			uci := CoordsToUcis(pos, captureAbs)
+			// promotion
+			if pawn.Color() == WHITE && captureAbs.r == 7 ||
+				pawn.Color() == BLACK && captureAbs.r == 0 {
+				pMap := promotionMap()
+				for pKey, pType := range pMap {
+					moveBoard := b.Copy()
+					moveBoard.MovePiece(pos, captureAbs)
+					moveBoard.SetPieceAt(captureAbs, NewPiece(pType, pawn.Color()))
+					moveBoard.RemoveEnPassant()
+					availableMoves[uci+pKey] = moveBoard
+				}
+			} else {
+				moveBoard := b.Copy()
+				moveBoard.MovePiece(pos, captureAbs)
+				moveBoard.RemoveEnPassant()
+				if pieceAtCapture.Type() == EnPassantType {
+					pawnEnPassantAbsolute := pawnEnPassantRelative.add(&captureAbs)
+					moveBoard.SetPieceAt(pawnEnPassantAbsolute, NewPiece(EmptyType, NONE))
+				}
+				availableMoves[uci] = moveBoard
+			}
 		}
-		availableMoves[CoordsToUcis(pos, rightCapture)] = moveBoard
 	}
 
 	return availableMoves
@@ -300,7 +329,6 @@ func KingMoves(b *Board, pos Coord, attackOnly bool) map[string]*Board {
 				}
 			}
 			if castlingIsLegal {
-				fmt.Println("castle is legal")
 				moveBoard := b.Copy()
 				moveBoard.MovePiece(pos, kingQMovementCoords[len(kingQMovementCoords)-1])
 				moveBoard.MovePiece(queenSideRookCoord, kingQMovementCoords[len(kingQMovementCoords)-2])
