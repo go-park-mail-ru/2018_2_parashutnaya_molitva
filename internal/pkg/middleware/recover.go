4449a1d2daf26fcf71ee6e73c42b5f896e9b50e2
Nikita Altunin <ksenobait_09@mail.ru>
2019-02-11 19:45:06 +0300

    proper log panic

--- internal/pkg/middleware/recover.go
+++ internal/pkg/middleware/recover.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-park-mail-ru/2018_2_parashutnaya_molitva/internal/pkg/singletoneLogger"
@@ -10,7 +11,7 @@ func RecoverPanicMiddleware(h http.Handler) http.Handler {
 	var mw http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if r := recover(); r != nil {
-				singletoneLogger.LogMessage("Recover")
+				singletoneLogger.LogMessage(fmt.Sprintf("Recover from error: %v", r))
 				http.Error(w, "Internal server error", http.StatusInternalServerError)
 			}
 		}()
